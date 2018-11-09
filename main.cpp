c13ae1f8e395b96c59858c64dab2282d759d7de5
linux_china <libing.chen@gmail.com>
2018-11-09 11:13:18 -0800

    polishing

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 #include <rxcpp/rx.hpp>
 
 int main() {
+    auto sequences = rxcpp::observable<>::from(1, 2, 3, 4);
+    sequences.subscribe([](int v) { printf("OnNext: %d\n", v); },
+                        []() {
+                            printf("OnCompleted sequences\n");
+                        });
+
     auto values = rxcpp::observable<>::range(1, 12)
             .filter([](int v) { return v % 2 == 0; })
             .map([](int x) { return x * x; });
     values.subscribe(
             [](int v) { printf("OnNext: %d\n", v); },
-            []() { printf("OnCompleted\n"); });
+            []() { printf("OnCompleted range\n"); });
 }
